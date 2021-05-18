75eca3942840d54921c261a8dbe41b9413520116
Shasha Yan <shashayan@gmail.com>
2021-05-18 02:54:17 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -10,21 +10,21 @@ class Transfer
   end
 
   def valid?
-    sender.bank_account.valid? && receiver.bank_account.valid?
+    sender.valid? && receiver.valid?
   end
 
   def execute_transaction
-    if valid? && sender.bank_account.balance > amount && self.status == "pending"
-      sender.bank_account.balance -= amount
-      receiver.bank_account.balance += amount
+    if valid? && sender.balance > amount && self.status == "pending"
+      sender.balance -= amount
+      receiver.balance += amount
       self.status = "complete"
     end
   end
 
   def reverse_transfer
-    if valid? && receiver.bank_account.balance > amount && self.status == "complete"
-      receiver.bank_account.balance -= amount
-      sender.bank_account.balance += amount
+    if valid? && receiver.balance > amount && self.status == "complete"
+      receiver.balance -= amount
+      sender.balance += amount
       self.status = "reversed"
     else
       reject_transfer
